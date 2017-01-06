85eba180b8a1686f9780dfcc0a5665cf8d8ff008
Nguyễn Tín <TinNguyen@Nguyens-MBP.net.fpt>
2017-01-06 14:34:17 +0700

    Test Git Push with New Collaborator

--- app/app.component.js
+++ app/app.component.js
@@ -11,12 +11,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Hello {{name}}</h1>",
+            template: "<h1>Hello Angular 2 & Spring Rest API</h1>",
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello Angular 2 & Spring Rest API</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {  }
