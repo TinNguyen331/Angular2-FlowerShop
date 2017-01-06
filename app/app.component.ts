b578dc5b59bb16249b589c658c913e074522f312
doanphong1995 <phongdvt95@gmail.com>
2017-01-06 15:21:38 +0700

    abc

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello Angular 2 & Spring Rest API</h1>`,
+  template: `<h1>LUU DAN CON BOM dhaskldhfsdklfhewuks dwassdasdasdasdas dwqsdwa</h1>`,
 })
 export class AppComponent  {  }
